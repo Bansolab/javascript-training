b4f0939179f1f7c55ca0e7accdc0ec08046132f3
yurika.yamaguchi <yurika.yamaguchi@bansolab.com>
2020-12-28 10:43:37 +0900

    Add:Calculation to console.log #80

--- javascripts/chapter01.js
+++ javascripts/chapter01.js
@@ -8,3 +8,12 @@ const sum = a + b;
 console.log(sum);
 
 console.log('こんにちは', '現在', new Date(), 'です');
+
+// ch1-5 四則演算
+console.log('--------ch1-5--------');
+console.log(100 + 200); //300
+console.log(200 - 80); //120
+console.log(100 * 3); //300
+console.log(400 / 5); //80
+console.log(402 % 5); //2
+console.log(2 ** 3); //8
